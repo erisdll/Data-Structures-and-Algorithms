acecfb9461b9364b5bc277691999350c2adc7c77
erisdll <72031082+erisdll@users.noreply.github.com>
2024-05-28 21:37:05 -0300

    push append done

--- Data Structures/01 Linked Lists/SinglyLL.c
+++ Data Structures/01 Linked Lists/SinglyLL.c
@@ -4,39 +4,35 @@
 typedef struct nodeStruct
 {
     int nodeValue;
-    struct nodeStruct *nextNode;
+    struct nodeStruct *nextNodePtr;
 } Node;
 
 Node *createList()
 {
     Node *headNode = (Node *)malloc(sizeof(Node));
-
     if (headNode == NULL)
     {
         fprintf(stderr, "Memory allocation failed!\n");
         exit(1);
     }
-
-    int inputValue;
     printf("Please insert node value:");
-    scanf("%d", &inputValue);
+    scanf("%d", &headNode->nodeValue);
 
-    headNode->nodeValue = inputValue;
-    headNode->nextNode = NULL;
+    headNode->nextNodePtr = NULL;
     return headNode;
 }
 
-void displayList(Node *headPointer)
+void displayList(Node *headPtr)
 {
-    Node *currentNode = headPointer;
-    if (headPointer == NULL)
+    Node *currentNode = headPtr;
+    if (headPtr == NULL)
         printf("The list is empty.\n");
     else
     {
         while (currentNode != NULL)
         {
             printf("%d -> ", currentNode->nodeValue);
-            currentNode = currentNode->nextNode;
+            currentNode = currentNode->nextNodePtr;
         }
 
         printf("NULL\n");
@@ -44,14 +40,14 @@ void displayList(Node *headPointer)
     printf("\n");
 }
 
-void countNodes(Node *headPointer)
+void countNodes(Node *headPtr)
 {
-    Node *currentNode = headPointer;
+    Node *currentNode = headPtr;
     int nodeCounter = 0;
-
+    
     while (currentNode != NULL)
     {
-        currentNode = currentNode->nextNode;
+        currentNode = currentNode->nextNodePtr;
         nodeCounter++;
     }
 
@@ -63,58 +59,55 @@ void countNodes(Node *headPointer)
         printf("The list has %d nodes.", nodeCounter);
 }
 
-void push_node(Node **headPointer)
+void pushNode(Node **headPtr)
 {
-    Node *headNode = (Node *)malloc(sizeof(Node));
-    int input_data;
-
-    if (headNode == NULL)
+    Node *newHeadPtr = (Node *)malloc(sizeof(Node));
+    if (newHeadPtr == NULL)
     {
-        printf("Memory allocation failed!\n");
-        return;
+        fprintf(stderr, "Memory allocation failed!\n");
+        exit(1);
     }
-
     printf("Insert node value:\n");
-    scanf("%d", &input_data);
+    scanf("%d", &newHeadPtr->nodeValue);
 
-    headNode->node_data = input_data;
-    headNode->next_node = *headPointer;
-
-    *headPointer = headNode;
+    newHeadPtr->nextNodePtr = *headPtr;
+    *headPtr = newHeadPtr;
 }
 
-/*
-void append_node(Node *headPointer)
+void appendNode(Node **headPtr)
 {
-    Node *headNode = (Node *)malloc(sizeof(Node));
-    Node *currentNode = headPointer;
-    int input_data;
-
-    if (headNode == NULL)
+    Node *curNodePtr = *headPtr;
+    Node *newNodePtr = (Node *)malloc(sizeof(Node));
+    if (newNodePtr == NULL)
     {
         printf("Memory allocation failed!");
     }
-
     printf("Insert node value:\n");
-    scanf("%d", &input_data);
+    scanf("%d", &newNodePtr->nodeValue);
 
-    headNode->node_data = input_data;
-    headNode->next_node = NULL;
+    newNodePtr->nextNodePtr = NULL;
 
-    while (currentNode->next_node != NULL)
+    while (curNodePtr->nextNodePtr != NULL)
     {
-        currentNode = currentNode->next_node;
+        curNodePtr = curNodePtr->nextNodePtr;
     }
 
-    currentNode->next_node = headNode;
+    curNodePtr->nextNodePtr = newNodePtr;
 }
 
-void insertNode(Node **headPointer)
+void insertNode(Node **headPtr)
 {
-    Node *headNode = (Node *)malloc(sizeof(Node));
-    int input_data;
+    Node *curNodePtr = *headPtr;
+    Node *newNodePtr = (Node *)malloc(sizeof(Node));
+    if (newNodePtr == NULL)
+    {
+        printf("Memory allocation failed!");
+    }
+    printf("Insert node value:\n");
+    scanf("%d", &newNodePtr->nodeValue);
 }
 
+/*
 void searchNode() {}
 
 void updateNode() {}
@@ -124,63 +117,87 @@ void deleteNode() {}
 void reverseList() {}
 */
 
+void insertNodeChoice(Node **headPtr)
+{
+    printf("╔════════════════════════════════╗\n");
+    printf("║ 1 — Insert at list beginning   ║\n");
+    printf("║ 2 — Insert at list ending      ║\n");
+    printf("║ 3 — Insert at specific index   ║\n");
+    printf("║ 4 — Insert before a value      ║\n");
+    printf("╚════════════════════════════════╝\n");
+
+    int insChoice;
+    scanf("%d", &insChoice);
+
+    switch (insChoice)
+    {
+    case 1:
+        pushNode(headPtr);
+        break;
+    
+    case 2:
+        appendNode(headPtr);
+        break;
+
+    default:
+        break;
+    }
+}
+
 void printMenu()
 {
-    printf("╔═════════════════════╦═════════════════════╗\n");
-    printf("║ 1 — Create a list.  ║  6 — Display list.  ║\n");
-    printf("║ 2 — Insert a node.  ║  7 — Count nodes.   ║\n");
-    printf("║ 3 — Search a node.  ║  8 — Reverse List   ║\n");
-    printf("║ 4 — Update a node.  ║  9 — Show Menu      ║\n");
-    printf("║ 5 — Delete a node.  ║  0 — Exit Program.  ║\n");
-    printf("╚═════════════════════╩═════════════════════╝\n");
+    printf("╔═════════════════════╦════════════════════╗\n");
+    printf("║ 1 — Create a list   ║ 6 — Display list   ║\n");
+    printf("║ 2 — Insert a node   ║ 7 — Count nodes    ║\n");
+    printf("║ 3 — Search a node   ║ 8 — Reverse List   ║\n");
+    printf("║ 4 — Update a node   ║ 9 — Show Menu      ║\n");
+    printf("║ 5 — Delete a node   ║ 0 — Exit Program   ║\n");
+    printf("╚═════════════════════╩════════════════════╝\n");
 }
 
 void main()
 {
-    Node *headPointer = NULL;
-    int choice;
+    Node *headPtr = NULL;
+    int choice = -1;
     do
     {
-        if (choice == NULL)
-        {
-            printMenu();
-        }
-
+        printMenu();
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
-            headPointer = createList();
-            displayList(headPointer);
+            headPtr = createList();
+            displayList(headPtr);
             break;
 
         case 2:
-            displayList(headPointer);
+            displayList(headPtr);
             break;
 
         case 3:
-            countNodes(headPointer);
+            insertNodeChoice(&headPtr);
+            choice = -1;
             break;
 
         case 4:
-            // insertNode(&headPointer);
+            // insertNode(&headPtr);
             break;
 
         case 5:
-            // searchNode(headPointer);
+            // searchNode(headPtr);
             break;
 
         case 6:
-            // updateNode(headPointer);
+            // updateNode(headPtr);
             break;
 
         case 7:
-            // deleteNode(&headPointer);
+            // deleteNode(&headPtr);
             break;
 
         case 8:
-            // reverseList(&headPointer);
+            // reverseList(&headPtr);
             break;
 
         case 9:
